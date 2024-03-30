4982337e61310674949aa9cb25072b02c33aa6e1
Daylen McCarron <daylenricardomccarron@gmail.com>
2024-03-30 16:05:22 +0530

    initail commit

---
+++ lib/view/bottomNavigationBar/bottomNavigationBar.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:persistent_bottom_nav_bar_v2/persistent_bottom_nav_bar_v2.dart';
+import 'package:quicknomsdriver/view/accountScreen/accountScreen.dart';
+import 'package:quicknomsdriver/view/historyScreen/historyScreen.dart';
+import 'package:quicknomsdriver/view/homeScreen/homeScreen.dart';
+
+class BottomNavigationBarQuickNoms extends StatefulWidget {
+  const BottomNavigationBarQuickNoms({super.key});
+
+  @override
+  State<BottomNavigationBarQuickNoms> createState() =>
+      _BottomNavigationBarQuickNomsState();
+}
+
+class _BottomNavigationBarQuickNomsState
+    extends State<BottomNavigationBarQuickNoms> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {});
+  }
+
+  PersistentTabController _controller =
+      PersistentTabController(initialIndex: 0);
+  @override
+  Widget build(BuildContext context) {
+    return PersistentTabView(
+      controller: _controller,
+      tabs: [
+        PersistentTabConfig(
+          screen: const HomeScreen(),
+          item: ItemConfig(
+            icon: const Icon(Icons.home),
+            title: "Home",
+          ),
+        ),
+        PersistentTabConfig(
+          screen: const HistoryScreen(),
+          item: ItemConfig(
+            icon: const Icon(Icons.list),
+            title: "History",
+          ),
+        ),
+        PersistentTabConfig(
+          screen: const AccountScreen(),
+          item: ItemConfig(
+            icon: const Icon(Icons.person),
+            title: "Account",
+          ),
+        ),
+      ],
+      navBarBuilder: (navBarConfig) => Style1BottomNavBar(
+        navBarConfig: navBarConfig,
+      ),
+    );
+  }
+}
